36787b10ae472fbc2666f3c325fb715ced8183d3
RDestailleurs <romain.destailleurs1@gmail.com>
2024-01-21 23:16:13 +0100

    ajout de contenu et tests (incomplet)

---
+++ ArtShop-frontend/src/Components/Connexion/Connexion.jsx
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import './Connexion.css';
+
+class Connexion extends Component{
+    render(){
+        return(
+            <div classname = "connexion">
+                <h1>Connexion</h1>
+            </div>
+        );
+    }
+}
+
+export default Connexion;
