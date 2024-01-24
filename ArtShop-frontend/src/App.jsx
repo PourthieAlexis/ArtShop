05836953791a85b111f0b062b205f44d6e85c83d
RDestailleurs <romain.destailleurs1@gmail.com>
2024-01-24 16:32:39 +0100

    ajout mineur sur les css

--- ArtShop-frontend/src/App.jsx
+++ ArtShop-frontend/src/App.jsx
@@ -1,6 +1,5 @@
 
 import "./App.css";
-//import Connexion from "./Components/Connexion/Connexion";
 import Menuheader from "./Components/menuheader/menuheader";
 import Accueil from "./Components/Accueil/Accueil";
 function App() {
