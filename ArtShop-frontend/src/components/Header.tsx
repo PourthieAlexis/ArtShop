422f9896f9575c7cc0ac63cffee7d68f39b8486f
Alexis <alexispourthie@gmail.com>
2024-04-02 11:51:17 +0200

    feat: add footer & header

--- ArtShop-frontend/src/components/Header.tsx
+++ ArtShop-frontend/src/components/Header.tsx
@@ -1,80 +1,90 @@
-import styled from 'styled-components';
-import SecondaryInput from './SecondaryInput';
-import { useSelector } from 'react-redux';
-import { selectIsLogged } from '../reducers/authenticationSlice';
-import { IoCartOutline } from 'react-icons/io5';
-import { CiUser } from 'react-icons/ci';
-import { BiCart, BiMessage, BiShoppingBag, BiUser } from 'react-icons/bi';
-export interface IHeaderProps {
-}
+import styled from "styled-components";
+import SecondaryInput from "./SecondaryInput";
+import { useSelector } from "react-redux";
+import { selectIsLogged } from "../reducers/authenticationSlice";
+import { BiMessage, BiShoppingBag, BiUser } from "react-icons/bi";
+export interface IHeaderProps {}
 
 export default function Header(props: IHeaderProps) {
-    const isAuthenticated = useSelector(selectIsLogged);
-    return (
-        <HeaderContainer>
-            <ListLink>
-                <Link>Art WorkShop</Link>
-            </ListLink>
-            <LogoImage>ArtShop</LogoImage>
-            <GroupButton>
-                {isAuthenticated
-                    ? <><StyledButton><CartButton /></StyledButton>
-                        <StyledButton><MessageButton /></StyledButton>
-                        <StyledButton><ProfilButton /></StyledButton>
-                    </>
-                    : <SecondaryInput type='button' value='Login' style={{ width: '7rem', height: '3rem' }} />}
-            </GroupButton>
-        </HeaderContainer>
-    );
+  const isAuthenticated = useSelector(selectIsLogged);
+  return (
+    <HeaderContainer>
+      <ListLink>
+        <Link>Art WorkShop</Link>
+      </ListLink>
+      <LogoImage>ArtShop</LogoImage>
+      <GroupButton>
+        {isAuthenticated ? (
+          <>
+            <StyledButton>
+              <CartButton />
+            </StyledButton>
+            <StyledButton>
+              <MessageButton />
+            </StyledButton>
+            <StyledButton>
+              <ProfilButton />
+            </StyledButton>
+          </>
+        ) : (
+          <SecondaryInput
+            type="button"
+            value="Login"
+            style={{ width: "7rem", height: "3rem" }}
+          />
+        )}
+      </GroupButton>
+    </HeaderContainer>
+  );
 }
 
 const HeaderContainer = styled.header`
-    display: flex;
-    justify-content: space-between;
-    padding: 0 2rem;
-    height: 5rem;
-`
+  display: flex;
+  justify-content: space-between;
+  padding: 0 2rem;
+  height: 5rem;
+`;
 
 const ListLink = styled.ul`
-    display: flex;
-    align-items: center;
-    padding: 0;
-`
+  display: flex;
+  align-items: center;
+  padding: 0;
+`;
 
 const Link = styled.li`
-    list-style: none;
-`
+  list-style: none;
+`;
 
 const LogoImage = styled.div`
-    display: flex;
-    align-items: center;
-`
+  display: flex;
+  align-items: center;
+`;
 
 const GroupButton = styled.div`
-    display: flex;
-    align-items: center;
-    gap: 1rem;
-`
+  display: flex;
+  align-items: center;
+  gap: 1rem;
+`;
 
 const StyledButton = styled.button`
-    display: flex;
-    align-items: center;
-    cursor: pointer;
-    padding: 0.7rem;
-    background-color: #E3E8F2;
-    color: #141C24;
-    border: none;
-    border-radius: 4px;
-`
+  display: flex;
+  align-items: center;
+  cursor: pointer;
+  padding: 0.7rem;
+  background-color: #e3e8f2;
+  color: #141c24;
+  border: none;
+  border-radius: 4px;
+`;
 
 const CartButton = styled(BiShoppingBag)`
-    font-size:1.5rem;
-`
+  font-size: 1.5rem;
+`;
 
 const MessageButton = styled(BiMessage)`
-    font-size:1.5rem;
-`
+  font-size: 1.5rem;
+`;
 
 const ProfilButton = styled(BiUser)`
-    font-size:1.5rem;
-`
+  font-size: 1.5rem;
+`;
